d05a3da5d5e7361c15ec7e072a97b2ae058d2530
Magdy-00 <114831143+Magdy-00@users.noreply.github.com>
2023-01-30 12:58:27 +0200

    Update Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,53 +1,80 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <cmath>
 using namespace std;
-int main(){
-    cout<<"If you wanna to take the square root op should be '|' \n";
-    cout<<"for power op should be '^' \n";
-    cout<<"+ for addition \n";
-    cout<<"- for subtraction \n";
-    cout<<"/ for division \n";
-    cout<<"* for multiplication \n";
-    cout<<"The op '|' only takes one number \n"; 
+int main()
+{
+    char confirmation;
+    confirmation = 'y';
+    cout << " '|' for square root \n";
+    cout << " '^' for power \n";
+    cout << " '+' for addition \n";
+    cout << " '-' for subtraction \n";
+    cout << " '/' for division \n";
+    cout << " '*' for multiplication \n";
+    cout << "The op '|' only takes one number \n";
     int num1;
     int num2;
     char op;
-    cout<<"Enter an operator: \n";
-    cin>>op;
-    if(op=='|'){
-        cout<<"Enter the number: \n";
-        cin>>num1;
+    while (confirmation == 'y')
+    {
+        cout << "Enter an operator: \n";
+        cin >> op;
+        if (op == '|')
+        {
+            cout << "Enter the number: \n";
+            cin >> num1;
+        }
+        else
+        {
+            cout << "Enter the frist number: \n";
+            cin >> num1;
+            cout << "Enter the second  number: \n";
+            cin >> num2;
+        }
+        switch (op)
+        {
+            // power operation
+        case '^':
+        {
+            cout << "The result is: " << pow(num1, num2) << endl;
+            break;
+        }
+            // square root operation
+        case '|':
+        {
+            cout << "The result is: " << sqrt(num1) << endl;
+            break;
+        }
+            // addition operation
+        case '+':
+        {
+            cout << "The result is: " << num1 + num2 << endl;
+            break;
+        }
+        case '/':
+            // division operation
+            {
+                cout << "The result is: " << num1 / num2 << endl;
+                break;
+            }
+        case '-':
+            // subtraction operation
+            {
+                cout << "The result is: " << num1 - num2 << endl;
+                break;
+            }
+        case '*':
+            // multiplication operation
+            {
+                cout << "The result is: " << num1 * num2 << endl;
+                break;
+            }
+        default:
+            cout << "Wrong operator\n";
+        }
+        cout << "Do you want to continue ?(y for yes, n for n) " << endl;
+        cin >> confirmation;
     }
-    else {
-    cout<<"Enter the frist number: \n";
-    cin>>num1;
-    cout<<"Enter the second  number: \n";
-    cin>>num2;
-    }
-    switch (op) {
-        //power operation
-    case '^':
-    cout<<pow(num1, num2);        
-        break;
-    case '|':
-    cout<<sqrt(num1);
-        break;
-    case '+': {
-    cout<<num1 + num2;
-    break;
-    }
-    case '/':
-    cout<<num1/num2;
-        break;
-    case '-':
-    cout<<num1-num2;
-        break;
-    case '*':
-    cout<<num1*num2;
-        break;
-    default:
-        cout<<"Wrong operator";
-}
 
     return 0;
 }
